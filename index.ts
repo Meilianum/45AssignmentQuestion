c5dd147c58acb48f3194189512c2b184f3fad320
Meilianum <164498186+Meilianum@users.noreply.github.com>
2024-06-04 04:41:06 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,291 @@
+// 45 assignment question
+
+//Q1
+// let message : string  = "Hello Eric whuld you like learn some python today?
+// console.log(message);
+              // (answer) , Hello Eric whuld you like learn some python today?
+//Q2
+
+ //
+               //(answer) Rukhsana
+//Q3
+// let  personName:   string = "Rukhsana";
+// console.log(personName.toUpperCase());
+// console.log(personName.toLowerCase());
+// console.log(personName .charAt(0) .toUpperCase () +personName.slice(1) .toLowerCase());
+    //(answer) RUKHSANA
+    //(anwer) rukhsana
+    //(answer)RUKHSANA
+
+   // Q4 : Q5
+// let famousperson : string ="Albert Estein"
+ //let message : string = `${famousperson}once said, “A person who never made a mistake never tried anything new.?`
+ //console.log(message);
+//Albert Esteinonce said, “A person who never made a mistake never tried anything new.?
+
+//Q6
+// let Name : string = "\t\n Rukhsana\t\n ";
+// console.log(Name)
+// console.log(Name.trim())
+  //Rukhsana //show the name withspace
+//Rukhsana  //Shows the name without whitespace
+
+//Q7 : Q8
+// addition
+// console.log(5+3);
+ // (answer) 8
+// subraction//
+//console.log(10-2);
+     //(answer) 8
+//multiplication //
+//console.log(4*2);
+   // (answer)  8 
+//division //
+//console.log(16/2);
+   //(answer)  8
+
+
+ //  Q9
+
+//  let favoriteNumber:number = 6;
+// let  messages =  'favoriteNumber Because its my birthday'
+// console.log( favoriteNumber, messages);
+//  (answer)   6 Because its my birthday
+
+//Q10
+// let myName: string = "Rukhsana";
+// let message: string =`${myName} , whould you like learn some typcript today?`
+// console.log(message);
+//   (answer q/no 10) Rukhsana , whould you like learn some typcript today now.?
+
+//Q11
+//  let friendname:  string[] =  ["Anum", "Anzila", "Mariyam"]
+//  for(let i=0; i<friendname.length; i++){
+
+//     console.log(friendname[i]); 
+//  }
+   //answer q/no11)anum
+//anzila
+//mariyam
+
+
+
+//Q12
+// let Names: string[] =["Anum", "Ayan" ,"Mekh", "Sabi"]
+// for(let name of Names){
+//     console.log(`Hello ${name}, "whouldyou like learn some typcript today?`)
+// }(answerq/no12)
+// Hello anum, "whouldyou like learn some typcript today ?
+// Hello ayan, "whouldyou like learn some typcript today ?
+// Hello mekh, "whouldyou like learn some typcript today ?
+// Hello sabi, "whouldyou like learn some typcript today ?
+
+//Q13
+//  let transports:string[] =['Honda mortorcyle', 'Tesla car',"Bianchi bicycle"];
+//  transports.forEach (transports => {
+ 
+//      console.log(`I whould like to own a ${transports}.`)
+// });
+//(answer q.no13)
+
+// let guest: string[] =["Amber", "anum", "anus"]
+// guest.forEach(guest =>{
+// console.log(`Dear ${guest}, i whould like to invite take dinner with me.`)
+// })
+//(answerq/14)
+// )
+// Dear Amber, i whould like to invite take dinner with me.
+// Dear anum, i whould like to invite take dinner with me.
+// Dear anus, i whould like to invite take dinner with me.
+
+//Q14 
+// let guest: string [] =["Albert Astein" , "Mari Curie", 'Nikola Tesla']
+//  guest .forEach(guest => {
+//    console.log(`Dear ${guest}, whould you like to joint me for dinner?`)
+// });
+// (answer no 14)     
+// Dear Albert Astein, whould you like to joint me for dinner?
+// Dear Mari Curie, whould you like to joint me for dinner?
+// Dear Nikola Tesla, whould you like to joint me for dinner?
+
+//Q15
+ //let  unableToAttend = "Nikol Tesla"
+ //console.log(`${unableToAttend}, can't  make it to dinner.`)
+//(answer 15)
+//Nikol Tesla, can't  make it to dinner.
+
+// let newGuest =   " Leonardo da vinci"
+// guest[guest.indexOf(unableToAttend)]  = newGuest;
+// //Dear  Leonardo da vinci, whould you like to join me for dinner
+
+// newGuest.forEACH => {
+//    console.log(`Dear ${newGuest},whould you like to joint me for dinner.`)
+
+
+
+
+// }
+//Q16
+//let guest: string [] = ["Albert Estin","Mari Curie", " Leonardo da vinci"]
+ //Dear Albert Estin,Mari Curie, Leonardo da vinci, whould you like to join me for dinner.
+//guest.pop()
+//console.log(` Dear ${guest}, whould you like to joint me for dinner. `)
+//(answer Q16) Dear Albert Estin,Mari Curie, whould you like to join me for dinner. 
+
+// let guest: string [] =  ["Albert Estin","Mari Curie", " Leonardo da vinci"]
+// //console.log(guest) //[ 'Albert Estin', 'Mari Curie', ' Leonardo da vinci' ]
+// //guest.unshift("isaac Newton")
+// //console.log(guest)  //[ 'isaac Newton', 'Albert Estin', 'Mari Curie', ' Leonardo da vinci' ]
+// guest.splice(guest.length/ 2, 0," Charles Darwin")
+// console.log(guest);
+// guest.push("Ada Lovelace")
+// guest.forEach(guest =>{
+// console.log(`Dear ${guest},whould you like to join me for dinner.`)
+//  })
+
+//Q17
+
+// console .log("I inform  everyone  i  got a big table ,so invite 3 more guest")
+// const addGuest:string[]  = ["Anum", 'Anus', "anaa"]
+// const  message = `Dear ${addGuest}, whould you like enjoy dinner with for me.`
+// //add new guest
+// addGuest.unshift("Saniya")
+// // adding guest and 
+// 
+// addGuest.push('Rukhsana')
+// addGuest.forEach(addGuest => {
+// console.log(`Dear ${addGuest}, whould you like enjoy dinner with for me.`) 
+// });
+
+//Q18
+// let  placeName: string [] = ["Banagladesh","Austaliya", 'Dubai',"Newzeland", "Europe"];
+// console.log("orignal order: placeNAME");
+// console.log("alphabetical order: placeName",[...placeName].sort());
+//  console.log("orignal order: placeName");
+// placeName.reverse
+// console.log("reverse order , placeName");
+// placeName.sort
+// console.log("alphabetical order", placeName);
+//  placeName.reverse()
+//  console.log("Reverse order", placeName );
+
+// (answerq18/) rignal order: placeNAME
+//  alphabetical order: placeName [ 'Austaliya', 'Dubai', 'Europe', 'banagladesh', 'newzeland' ]
+//  orignal order: placeName
+//  reverse order , placeName
+//  alphabetical order [ 'banagladesh', 'Austaliya', 'Dubai', 'newzeland', 'Europe' ]
+//  Reverse order [ 'Europe', 'newzeland', 'Dubai', 'Austaliya', 'banagladesh' ]
+
+//Q19
+//  console.log(" I want to  invite aleast 4to6 guests for dinner ejoying with me.")
+// let guest : string [] = ["Farhan",  'bilal',  "sumera",  'Samra',  'Haseeb', "imran"];
+// let message:  "I feel happiness i invite for respecteable guest  on dinner with me."
+// guest.splice(0,1,'bisma',  " raniya" )
+// guest. forEach(guest =>{
+// console.log(`Dear ${guest},I feel happy  to invite such recpectful  guest  on dinner with me.`)
+//  }
+//  )
+// (answerq19) I want to  invite aleast 4to6 guests for dinner ejoying with me.
+//  I want to  invite aleast 4to6 guests for dinner ejoying with me.
+// Dear bisma,I feel happy  to invite such recpectful  guest  on dinner with me.  
+// Dear  raniya,I feel happy  to invite such recpectful  guest  on dinner with me.
+// Dear bilal,I feel happy  to invite such recpectful  guest  on dinner with me.  
+// Dear sumera,I feel happy  to invite such recpectful  guest  on dinner with me.
+// Dear Samra,I feel happy  to invite such recpectful  guest  on dinner with me.
+// Dear Haseeb,I feel happy  to invite such recpectful  guest  on dinner with me.
+// Dear imran,I feel happy  to invite such recpectful  guest  on dinner with me.
+
+//Q21
+// let  language: string [] =["Engilh","Spanish","German","Turkish","Romanh","Japness"]
+//    console.log(language );
+// [ 'Engilh', 'Spanish', 'German', 'Turkish', 'Romanh', 'Japness' ]
+
+//Q22.
+// let myBio : string | number| boolean|{
+//     name:    "Rukhsana",
+//     age:      20,
+//     phone :   33452647980,
+//     adress:    "abc street house no 1/16/3 nazmabad 1no",
+//}
+
+//Q22 object
+// let myBio= {
+//    name:    "samreen",
+//    age:      21,
+//    collegeName:  "abc public girls degree college ",
+//    rollNo:     2345,
+// }
+//    console.log(myBio)
+ 
+   // {
+   //    name: 'samreen',
+   //    age: 21,
+   //    collegeName: 'abc public girls degree college ',
+   //    rollNo: 2345
+   //  }
+   
+//22 
+//let  friedsName: string [] =["anum", "samiya", "badla" ,'yameen']
+//console.log(friedsName[4]);    
+//(answerq22/)
+// [ 'anum', 'samiya', 'badla', 'yameen' ]
+//console.log(friendsName[2])
+//badla   
+//console.log(friedsName[4])
+//undefined
+    
+
+ //Q23
+//  let car = "subaru";
+//  if(true){
+   
+//  console.log ("Is car == 'subaru'? I predict True.")
+//  console.log(car == 'subaru')
+//  }else{
+//    console.log(false)
+// };
+//(answer23q/)Is car == 'subaru'? I predict True.
+// false
+
+// C:\Users\HP\Desktop\45 Assignment>tsc && node index.js
+// Is car == 'subaru'? I predict True.
+// false
+
+// C:\Users\HP\Desktop\45 Assignment>tsc && node index.js
+// Is car == 'subaru'? I predict True.
+// true
+
+// C:\Users\HP\Desktop\45 Assignment>tsc && node index.js
+// Is car == 'subaru'? I predict True.
+// true
+// Is car == 'subaru'? I predict True.
+// false
+
+//Q24
+///let test : string  [] =["5", "10", "15", "20", "25","30",'35' , '40','50'];
+// let fruits: string [] = ["Apple", 'Mango', "orange", 'kiewi', "banana"]
+// let  apple: String [] = ["apple is color red season : winter tests is amzing"]
+//    console.log(5>=10)
+//    console.log(5<=10)  console.log(15===10)
+ //} console.log(15===15)
+   // if(5< 10 ){
+   // console.log(vagetable)
+ //}else{
+   //console.log("is wronge input")
+//}
+// if (true){
+//    console.log("Is right fruit of winter.")
+// }if (false)
+//    console.log('apple')
+//  else{
+// console.log('');
+// }
+//   let str = "Apple IS  Round && Color is Red  |. Season : Winte."
+  
+// console.log(str.toLowerCase())
+//Q25
+//let alien :  string [] =[ " an alien was shot down"]
+// let alien : string = "Alien"
+// let  message =  " alien was shot down  & his color is red."
+// console.log (`${message}`)
+
